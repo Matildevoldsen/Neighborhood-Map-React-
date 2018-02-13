cffa991a0a24059c9e81e31da2772df725dc8c12
Matilde Wittrup <matildeene@outlook.com>
2018-02-13 20:18:44 +0100

    Created functions

--- src/App.js
+++ src/App.js
@@ -1,51 +1,178 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Map from './Components/Map'
+import Sidebar from "./Components/Sidebar";
 
 class App extends Component {
-  constructor() {
-  	super();
-  	this.initMap = this.initMap.bind(this);
-  }
 
-  componentDidMount() {
-  	window.initMap = this.initMap;
-  	createMapLink('https://maps.googleapis.com/maps/api/js?key=AIzaSyAD4vpwyw4zFgzo_4_RG4lAaVwCIVZM9Jc&callback=initMap');
-  }
+    constructor(props) {
+        super(props);
 
-  initMap() {
-	let map;
-	map = new window.google.maps.Map(document.getElementById('map'), {
-		zoom: 13,
-		center: {lat: 51.509865, lng: -0.118092}
-	});  	
-  }
+        this.state = {
+            map: '',
+            info: '',
+            markers: [
+                {
+                    lat: 51.519946,
+                    long: -0.109537,
+                    name: 'Prufrock Coffee'
+                },
+                {
+                    lat: 51.509620,
+                    long: -0.126939,
+                    name: 'Notes',
+                },
+                {
+                    lat: 51.511060,
+                    long: -0.084289,
+                    name: 'The New Black'
+                },
+                {
+                    lat: 51.521918,
+                    long: -0.119791,
+                    name: 'Espresso Room'
+                },
+                {
+                    lat: 51.513619,
+                    long: -0.079086,
+                    name: 'The Association'
+                },
+                {
+                    lat: 51.519300,
+                    long: -0.140725,
+                    name: 'Attendant'
+                },
+                {
+                    lat: 51.512371,
+                    long: -0.127092,
+                    name: 'Coffee Island'
+                }
+            ],
+            virtualMarkers: []
+        };
 
-  render() {
-    return (
-	    <div>	
-	    	<header>
-	    		<h1>Neightboor Map</h1>
-	    	</header>
-		    <div id="map-container">
-		      	<div id="map" role="application">
-		      	</div>
-		    </div>
-	    </div>
-    );
-  }
+
+        this.initMap = this.initMap.bind(this);
+        this.generateMarkers = this.generateMarkers.bind(this);
+        this.openMarker = this.openMarker.bind(this);
+    }
+
+
+    componentDidMount() {
+        window.initMap = this.initMap;
+        createMapLink('https://maps.googleapis.com/maps/api/js?key=AIzaSyAD4vpwyw4zFgzo_4_RG4lAaVwCIVZM9Jc&callback=initMap');
+    }
+
+    initMap() {
+        let map;
+        map = new window.google.maps.Map(document.getElementById('map'), {
+            zoom: 13,
+            center: {lat: 51.509865, lng: -0.118092}
+        });
+
+        var infowindow = new window.google.maps.InfoWindow({});
+
+        this.setState({map: map, info: infowindow});
+        this.generateMarkers(map);
+    }
+
+    generateMarkers(map) {
+        let self = this;
+
+        this.state.markers.forEach(marker => {
+            const loc = {lat: marker.lat, lng: marker.long}
+
+            let mark = new window.google.maps.Marker({
+                position: loc,
+                map: map,
+                title: marker.name
+            });
+
+
+            mark.addListener('click', function () {
+                self.openMarker(mark);
+            });
+
+            let virtMarker = this.state.virtualMarkers;
+            virtMarker.push(mark);
+
+            this.setState({virtualMarkers: virtMarker});
+        });
+    }
+
+    openMarker(marker = '') {
+        const clientId = "VVPEFJC40SJDVH1YFRJS4IBNQ0GGZJY5X1XLHEA23H1LTVOQ\n";
+        const clientSecret = "MEAM2N42L434P1MB1AJZFUHM5XAGMCDNGETUH5XNZIYEHOKI\n";
+        const url = "https://api.foursquare.com/v2/venues/search?client_id=" + clientId + "&client_secret=" + clientSecret + "&v=20130815&ll=" + marker.getPosition().lat() + "," + marker.getPosition().lng() + "&limit=1";
+
+
+        if (this.state.info.marker != marker) {
+            this.state.info.marker = marker;
+            this.state.info.open(this.state.map, marker);
+            marker.setAnimation(window.google.maps.Animation.DROP);
+
+
+            this.state.info.addListener('closeClick', function () {
+                this.state.info.setMarker(null);
+            });
+
+            this.markerInfo(url);
+        }
+    }
+
+    markerInfo(url) {
+        let self = this.state.info;
+        let place;
+        fetch(url)
+            .then(function (resp) {
+                if (resp.status !== 200) {
+                 this.state.info.setContent("Can't load data.");
+                }
+                resp.json().then(function (data) {
+                    var place = data.response.venues[0];
+                    var open = "<p>" + place.verified + "</p>";
+                    var location = "<p>" + place.location.address + "</p>";
+                    self.setContent(open + location);
+                });
+
+                console.log(place);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <header>
+                    <Sidebar
+                        infoWindow={this.state.info}
+                        openInfo={this.openMarker}
+                        virtualMarker={this.state.virtualMarkers}
+                    >
+
+                    </Sidebar>
+                    <h1>Londons Cafe's</h1>
+                </header>
+                <Map markers={this.state.markers}></Map>
+            </div>
+        );
+    }
 }
 
 function createMapLink(url) {
-	let tag = window.document.getElementsByTagName('script')[0];
-	let script = window.document.createElement('script');
+    let tag = window.document.getElementsByTagName('script')[0];
+    let script = window.document.createElement('script');
 
-	script.src = url;
-	script.async = true;
-	script.onerror = function () {
+    script.src = url;
+    script.async = true;
+    script.onerror = function () {
         document.write("Google Maps can't be loaded");
     };
-	tag.parentNode.insertBefore(script, tag);
+    tag.parentNode.insertBefore(script, tag);
 }
 
 export default App;
